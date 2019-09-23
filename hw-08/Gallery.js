b4ed4465132578f8a7bc2662f226a626c4766dcb
MakotoTEach <teachermako@gmail.com>
2019-09-23 15:22:19 +0300

    HW-8 DONE

--- hw-08/Gallery.js
+++ hw-08/Gallery.js
@@ -6,6 +6,18 @@ const gallery = document.querySelector(".gallery");
 const lightBoxImage = document.querySelector(".lightbox___image");
 const lightBox = document.querySelector(".lightbox");
 const LightBoxButton = document.querySelector(".lightbox__button");
+const lightboxContent = document.querySelector(".lightbox__content");
+
+gallery.addEventListener("click", handleImageClick);
+LightBoxButton.addEventListener("click", closeLightBox);
+lightboxContent.addEventListener("click", handleLightBoxClick);
+
+function handleKeyPress(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+  closeLightBox();
+}
 
 const createGalleryItem = ({ preview, original, description }) => {
   const galleryItem = `<li class="gallery__item">
@@ -27,26 +39,33 @@ const createGalleryItem = ({ preview, original, description }) => {
 
   return galleryItem;
 };
-
 const createGallery = array => {
   return array.map(item => createGalleryItem(item)).join("");
 };
 
 gallery.insertAdjacentHTML("beforeEnd", createGallery(imagesArray));
 
-const showImage = event => {
+function openLightBox() {
+  lightBox.classList.add("is-open");
+  window.addEventListener("keydown", handleKeyPress);
+}
+function closeLightBox() {
+  lightBox.classList.remove("is-open");
+  window.removeEventListener("keydown", handleKeyPress);
+}
+function handleImageClick(event) {
   event.preventDefault();
-  let clickedElement = event.target.parentNode;
+  const clickedElement = event.target.parentNode;
   if (clickedElement.tagName != "A") {
     return;
   }
   lightBoxImage.src = clickedElement.href;
-  lightBox.classList.add("is-open");
-};
-
-const closeLightBox = event => {
-  lightBox.classList.remove("is-open");
-};
-
-gallery.addEventListener("click", showImage);
-LightBoxButton.addEventListener("click", closeLightBox);
+  openLightBox();
+}
+function handleLightBoxClick(event) {
+  const targetTagName = event.target.nodeName;
+  if (targetTagName === "IMG") {
+    return;
+  }
+  closeLightBox();
+}
