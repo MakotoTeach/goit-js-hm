3d77165133d664df092aaa358018b1b8c486f49d
MakotoTEach <teachermako@gmail.com>
2019-09-23 11:07:04 +0300

    HW7 DONE

--- hw-07/js/Task_08.js
+++ hw-07/js/Task_08.js
@@ -5,7 +5,7 @@ const buttonRender = document.querySelector('button[data-action="render"]');
 const buttonDestroy = document.querySelector('button[data-action="destroy"]');
 const boxes = document.querySelector("#boxes");
 
-const checkInput = event => {
+const checkInput = () => {
   let amount = Number(input.value);
 
   buildList(amount);
@@ -14,14 +14,24 @@ const checkInput = event => {
 const buildList = amount => {
   let template = "";
 
+  let size = 30;
+
   for (let i = 0; i < amount; i++) {
-    template += `<div class ="class"></div>`;
+    let color = randomColor(255);
+    template += `<div style ="width: ${size}px; height: ${size}px; background-color: ${color}"></div>`;
+    size += 10;
   }
   boxes.insertAdjacentHTML("afterbegin", template);
 };
 
 const destroy = () => {
   boxes.innerHTML = "";
+};
+const randomColor = number => {
+  const r = Math.floor(Math.random() * (number + 1));
+  const g = Math.floor(Math.random() * (number + 1));
+  const b = Math.floor(Math.random() * (number + 1));
+  return "rgb(" + r + "," + g + "," + b + ")";
 };
 
 buttonRender.addEventListener("click", checkInput);
