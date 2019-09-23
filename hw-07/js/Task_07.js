d7f6c132a762c128d8a5b35cb1792c916b449c18
MakotoTEach <teachermako@gmail.com>
2019-09-23 10:33:47 +0300

    dsadas

--- hw-07/js/Task_07.js
+++ hw-07/js/Task_07.js
@@ -2,11 +2,8 @@
 
 const slider = document.getElementById("font-size-slider");
 const text = document.getElementById("text");
-<<<<<<< HEAD
-=======
 text.style.fontSize = `${slider.value}px`;
 
->>>>>>> 4d622cbf9c1d10d054b2ccca733620d9e9412483
 const checkInput = event => {
   let check = Number(event.currentTarget.value);
   text.style.fontSize = `${check}px`;
