7dada51e8996e3e3d85d03b4b18b8a33cf0adf7c
MakotoTEach <teachermako@gmail.com>
2019-09-30 15:08:38 +0300

    HW-6

--- hw-06/js/task_10.js
+++ hw-06/js/task_10.js
@@ -1,91 +1,6 @@
 "use strict";
 
-const users = [
-  {
-    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37
-  },
-  {
-    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34
-  },
-  {
-    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24
-  },
-  {
-    id: "249b6175-5c30-44c6-b154-f120923736f5",
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21
-  },
-  {
-    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27
-  },
-  {
-    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38
-  },
-  {
-    id: "e1bf46ab-7168-491e-925e-f01e21394812",
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39
-  }
-];
+import users from "./users.js";
 
 const getSkills = users =>
   users
@@ -93,48 +8,7 @@ const getSkills = users =>
       allSkills.push(...user.skills);
       return allSkills;
     }, [])
-    // .filter((item, i, arr) => arr.indexOf(item) === i)
-
-    .filter((skill => skill!==skill))
+    .filter((item, i, arr) => arr.indexOf(item) === i)
     .sort();
 
 console.log(getSkills(users));
-
-// console.log(getSkills(users));
-
-// const skills = getSkills(users);
-// let totalSkills = [];
-
-// const getSkillsStat = (acc, skill) => {
-//   if (!acc.includes(skill)) {
-//     totalSkills.push(skill);
-//   }
-//   return totalSkills;
-// };
-
-// // console.log(getSkillsStat);
-
-// const amount = skills => skills.reduce(getSkillsStat, []);
-
-// console.log(amount(skills));
-
-// const getSkills = users.map(user => {
-//   return user.skills;
-// });
-
-// console.log(getSkills);
-
-// let total = [];
-
-// for (let i = 0; i < getSkills.length; i += 1) {
-//   console.log(`i:`, i);
-//   console.log(`getSkills[i]`, getSkills[i]);
-
-//   for (let j = 0; j < getSkills[i].length; j += 1) {
-//     console.log(`getSkills[i][j]`, getSkills[i][j]);
-//     if (!total.includes(getSkills[i][j])) {
-//       total.push(getSkills[i][j]);
-//     }
-//   }
-// }
-// console.log(total);
