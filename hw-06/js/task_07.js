31a702202daa5fe03eccc5650eeffa28c1b5ef59
MakotoTEach <teachermako@gmail.com>
2019-10-07 09:55:07 +0300

    AAA

--- hw-06/js/task_07.js
+++ hw-06/js/task_07.js
@@ -1,10 +1,6 @@
 "use strict";
 
-<<<<<<< HEAD
-import users from "./users";
-=======
 import users from "./users.js";
->>>>>>> d1d459d1518a7d21a0d930937fe2bfb2cf00d9ee
 
 const calculateTotalBalanceUsers = users =>
   users.reduce((totalBalance, users) => totalBalance + users.balance, 0);
