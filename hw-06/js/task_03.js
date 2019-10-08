0d9faefa33371633d5f426162cfcfbd8f8475cde
Makoto <Teachermako@gmail.com>
2019-10-08 19:05:34 +0300

    dsadas

--- hw-06/js/task_03.js
+++ hw-06/js/task_03.js
@@ -1,10 +1,6 @@
 "use strict";
 
-<<<<<<< HEAD
-import users from "./users";
-=======
 import users from "./users.js";
->>>>>>> d1d459d1518a7d21a0d930937fe2bfb2cf00d9ee
 
 const getUsersWithGender = (users, gender) =>
   users.filter(user => user.gender === gender);
