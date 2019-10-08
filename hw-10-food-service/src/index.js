9682cd694da6485e66553bd7553fb89d354b94b9
MakotoTEach <teachermako@gmail.com>
2019-10-08 14:12:01 +0300

    HW-10 DONE

--- hw-10-food-service/src/index.js
+++ hw-10-food-service/src/index.js
@@ -4,19 +4,48 @@ import templateItem from './templates/post-feed-item.hbs';
 
 const refs = {
   menu: document.querySelector('.menu'),
+  body: document.querySelector('body'),
+  themeSwitchControl: document.querySelector('#theme-switch-control'),
 };
 
-console.log(templateItem);
-console.log(menu);
+refs.themeSwitchControl.addEventListener('change', themeSwitch);
 
-// const markup = templateItem(menu[5]);
+function buildMenu(arr) {
+  const markup = arr.map(item => templateItem(item)).join('');
+  refs.menu.insertAdjacentHTML('beforeend', markup);
+}
+buildMenu(menu);
 
-// refs.menu.insertAdjacentHTML('beforeend', markup);
+const themeLocal = localStorage.getItem('theme');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-function buildMenu(menu) {
-const markup = menu.map(menuItem => templateItem(menuItem)).join('');
-
-refs.menu.insertAdjacentHTML('beforeend', markup);
+if (themeLocal === Theme.DARK) {
+  changeThemeForDark();
 }
 
-buildMenu(menu);
+function themeSwitch(e) {
+  if (e.target.checked) {
+    changeThemeForDark();
+    console.log(theme);
+    return;
+  }
+  changeThemeForLigth();
+  console.log(theme);
+}
+
+function changeThemeForLigth(e) {
+  refs.body.classList.remove(Theme.DARK);
+  refs.body.classList.add(Theme.LIGHT);
+  refs.themeSwitchControl.checked = false;
+  localStorage.setItem('theme', Theme.LIGHT);
+}
+
+function changeThemeForDark(e) {
+  refs.body.classList.remove(Theme.LIGHT);
+  refs.body.classList.add(Theme.DARK);
+  refs.themeSwitchControl.checked = true;
+  localStorage.setItem('theme', Theme.DARK);
+}
