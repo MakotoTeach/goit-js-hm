ac0d827315b99a589173c5e27e226f64fa35ceb8
MakotoTEach <teachermako@gmail.com>
2019-10-08 14:52:24 +0300

    HW-6 Changed

--- hw-06/js/task_03.js
+++ hw-06/js/task_03.js
@@ -3,6 +3,7 @@
 import users from "./users.js";
 
 const getUsersWithGender = (users, gender) =>
-  users.filter(user => user.gender === gender);
+  users.filter(user => user.gender === gender)
+  .map(user => user.name);
 
 console.log(getUsersWithGender(users, "male"));
