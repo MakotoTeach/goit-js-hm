0d46afa8a2a9cf9b0cd34cde1bd5f2c13de08c1f
Makoto <Teachermako@gmail.com>
2019-10-17 18:22:32 +0300

    HW11 1st Task

---
+++ hw-11-color-switch/js/color-switch.js
@@ -0,0 +1,61 @@
+// const a = 2;
+// const b = 3;
+
+// function sum(a, b) {
+// let result = a + b;
+// return result;
+// }
+
+// // setTimeout(() => {
+// //     console.log(sum(a, b));
+// // }, 2000);
+
+// let timer =  setInterval(() => {
+//     console.log(sum(a, b));
+// }, 2000);
+
+// setTimeout(() => {
+//     clearInterval(timer)
+// }, 11000);
+
+const refs = {
+  body: document.querySelector("body"),
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]')
+};
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548"
+];
+
+let randomColor = 0;
+let isActive = false;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const startShowColor = () => {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  randomColor = setInterval(() => {
+    let color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    console.log(color);
+    refs.body.style.backgroundColor = color;
+  }, 1000);
+};
+
+const stopShow = () => {
+  isActive = false;
+  clearInterval(randomColor);
+};
+
+refs.start.addEventListener("click", startShowColor);
+refs.stop.addEventListener("click", stopShow);
